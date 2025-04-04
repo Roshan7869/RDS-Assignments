19a3fe5f23d5b15f90844f6844f40f747b1a211d
Roshan7869 <khatriroshan7869@gmail.com>
2025-04-05 01:52:00 +0530

    c

---
+++ Assignment_3/Q1.r
@@ -0,0 +1,3 @@
+# Creating a logical vector of length 12 alternating between TRUE and FALSE
+logical_vector <- rep(c(TRUE, FALSE), length.out = 12)
+print(logical_vector)
---
+++ Assignment_3/Q2.r
@@ -0,0 +1,4 @@
+# Input numeric vector
+numeric_vector <- c(-5, 3, -2, 7, 0, -8)
+logical_vector <- numeric_vector < 0
+print(logical_vector)
